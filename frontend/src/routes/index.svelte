4a326968c9a52498050d97d9e13ef380b09d91be
John Rinehart <johnrichardrinehart@gmail.com>
2021-12-01 13:54:20 +0200

    chore(/src/routes/index.svelte): remove unused CSS selectors

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -102,24 +102,11 @@
 		}
 	}
 
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
 	.banner__info {
 		max-width: 100%;
 		margin-top: 90px;
 	}
-	.banner__info h1,
-	.banner__info h2,
-	.banner__info h3,
-	.banner__info h4,
-	.banner__info h5,
-	.banner__info h6 {
+	.banner__info h1 {
 		margin-bottom: 0;
 	}
 
