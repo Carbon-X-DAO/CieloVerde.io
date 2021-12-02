50c56eb2d635868455a32c400f9a2ac21991ec82
Ivan Ivanischev <ivanischev@MacBook-Pro-Ivan-2.local>
2021-12-02 16:37:55 +0200

    changed favicons

--- frontend/src/lib/header/Header.svelte
+++ frontend/src/lib/header/Header.svelte
@@ -9,8 +9,10 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
 	<link rel="manifest" href="/images/favicons/site.webmanifest">
-	<link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#000000">
-	<meta name="msapplication-TileColor" content="#00a300">
+	<link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#318048">
+	<link rel="shortcut icon" href="/images/favicons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-config" content="/images/favicons/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
 </svelte:head>
 
