3d94a1063c503f0f818774da2e061bffc62c4e85
John Rinehart <johnrichardrinehart@gmail.com>
2021-12-05 19:34:21 +0200

    chore: clean up the CSS for the overlay

--- fileserver/ticket.go
+++ fileserver/ticket.go
@@ -7,12 +7,12 @@ var ticketHTMLTemplate = `
 		window.alert("Save this page as PDF to save your ticket!")
 	</script>
 	<body>
-		<div id="flyer" style="position: relative;">
-			<img src="/tickets/flyer.jpg" width="1100px" height="1100px">
-			<div id="frame" style="position: absolute; display: flex; align-items: center; justify-content: center; width: 170px; height: 170px; border: dashed red; bottom: 81px; left: 497px;">
-				<img src="%s" style="width: 155px; height: 155px;">
-			</div>
+	<div id="flyer" style="position: relative;">
+		<img src="/tickets/flyer.jpg" width="1100px" height="1100px">
+		<div id="frame" style="position: absolute;display: flex;align-items: center;justify-content: center;width: 170px;height: 170px;border: dashed red;bottom: 80px;left: 495px;border-width: thick;">
+			<img src="%s" style="width: 160px; height: 160px;">
 		</div>
+	</div>
 	</body>
 </html>
 `
